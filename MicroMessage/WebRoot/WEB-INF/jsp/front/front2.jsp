f661a5963e8a9975ab80ba32e680ab83af9ce4b7
陈鸿健 <13552666934@139.com>
2016-06-04 12:22:37 +0800

    调整了ajax调用，和编码过滤器 20160604

--- MicroMessage/WebRoot/WEB-INF/jsp/front/front2.jsp
+++ MicroMessage/WebRoot/WEB-INF/jsp/front/front2.jsp
@@ -1,15 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <%
 	String path = request.getContextPath();
 	String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
 %>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>学习MyBatis页面</title>
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>新 增</title>
 <script src="<%= basePath %>resources/js/common/jquery-1.8.0.min.js" type="text/javascript"></script>
+
 </head>
 <body>
 	<input type="hidden" value="<%= basePath %>" id="basePath"/>
@@ -41,6 +42,5 @@
 			});
 		});
 	});
-	
 </script>
 </html>
--- MicroMessage/src/com/imooc/servlet/AskServlet.java
+++ MicroMessage/src/com/imooc/servlet/AskServlet.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+
 import com.alibaba.fastjson.JSON;
 import com.imooc.service.AskService;
 
@@ -15,16 +17,20 @@ import com.imooc.service.AskService;
  */
 public class AskServlet extends HttpServlet {
 
+	private Logger logger = Logger.getLogger(AskServlet.class);
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//设备请求编码集
-		req.setCharacterEncoding("utf-8");
+		//req.setCharacterEncoding("utf-8");
 		//调用service类
 //		req.setAttribute("list", JSON.toJSONString(new AskService().queryListByIn()));
-		resp.getWriter().println(JSON.toJSONString(new AskService().queryListByIn()));
-		req.getRequestDispatcher("/WEB-INF/jsp/front/front.jsp").forward(req, resp);
+		resp.setContentType("application/json");
+		String jsonStr = JSON.toJSONString(new AskService().queryListByIn());
+		logger.debug(">>>>  debug >>> " + jsonStr);
+		resp.getWriter().write(jsonStr);
+		//req.getRequestDispatcher("/WEB-INF/jsp/front/front.jsp").forward(req, resp);
 	}
 
 	@Override
